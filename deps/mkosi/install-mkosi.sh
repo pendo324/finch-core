3bb57406b5077c7c3f8331a2076ea93f42bbcd8b
Justin Alvarez <alvajus@amazon.com>
2025-11-17 11:13:01 -0500

    fix install mkosi script
    Signed-off-by: Justin Alvarez <alvajus@amazon.com>

--- deps/mkosi/install-mkosi.sh
+++ deps/mkosi/install-mkosi.sh
@@ -7,6 +7,9 @@ pushd mkosi
 git checkout 489c5e9ecc9f1185ef64636c5c29d8d0e59746e0
 git apply ./../deps/mkosi/0001-Add-support-for-Amazon-Linux-2023.patch
 popd
+mkdir -p "$HOME/.local/bin"
 ln -s $PWD/mkosi/bin/mkosi ~/.local/bin/mkosi
+echo "export PATH=$HOME/.local/bin:$PATH" >> ~/.bashrc
+export PATH="$HOME/.local/bin:$PATH"
 
 mkosi --version
